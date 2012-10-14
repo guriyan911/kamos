50892cb409264d62b969e4a346376ce5dcd0ac23
tatsuya yano <yano.tatsuya@nifty.com>
2012-10-14 14:32:30 +0900

    よくわからんけどコミット

---
+++ misekime/src/main/java/yuraku/misekime/App.java
@@ -0,0 +1,13 @@
+package yuraku.misekime;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ misekime/src/test/java/yuraku/misekime/AppTest.java
@@ -0,0 +1,14 @@
+package yuraku.misekime;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest
+{
+	@Test
+	public void appTest()
+    {
+        assertTrue(true);
+    }
+}
